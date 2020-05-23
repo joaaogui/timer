8c8943e567dead6c8224270fd59dba9477fee770
João Guilherme <joaaogui@gmail.com>
2020-05-23 18:35:26 -0300

    Adds vue

---
+++ src/components/Timer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="timer">
+        <p id="timespan">{{time}}</p>
+        <v-btn color="yellow" class="reset-button" v-on:click="select">Reset</v-btn>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Timer",
+
+    data: () => ({
+      time: 0
+    }),
+    methods: {
+      select: function () {
+        console.log("borabora")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .timer {
+        text-align: center;
+        font-size: 150px;
+    }
+</style>
