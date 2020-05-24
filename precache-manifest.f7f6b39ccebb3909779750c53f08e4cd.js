c9486b3231259623442ffa1170fe00a0dc0fa85e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-24 00:26:26 +0000

    Deploy timer.joaoguilherme.me to github.com/joaaogui/timer.git:gh-pages

---
+++ precache-manifest.f7f6b39ccebb3909779750c53f08e4cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c9746aa85dcd27ac63",
+    "url": "/css/app.203a5117.css"
+  },
+  {
+    "revision": "1fd2d2a4df08fc056b28",
+    "url": "/css/chunk-vendors.cc2a0755.css"
+  },
+  {
+    "revision": "1b7499b66dc1b7439bc807f02f6fe6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c9746aa85dcd27ac63",
+    "url": "/js/app.6a3504a3.js"
+  },
+  {
+    "revision": "1fd2d2a4df08fc056b28",
+    "url": "/js/chunk-vendors.98b41fe4.js"
+  },
+  {
+    "revision": "e8666afd43993de183c5515bd2d13e43",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
